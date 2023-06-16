6da32c36bad5309a90bcbcc0abb150709d636418
claudio batista <114784823+Starf1sh@users.noreply.github.com>
2023-06-16 00:50:31 -0300

    Add files via upload

---
+++ projeto final/controle/ctr_produto.php
@@ -0,0 +1,30 @@
+<?php
+    require_once'../modelo/produto.php';
+    $objProduto = new Produto();
+
+    if(isset($_POST['insert'])){
+        $nome = $_POST['txtnome'];
+        $quantidade = $_POST['txtquantidade'];
+        $valor = $_POST['txtvalor'];
+        if($objProduto->insert($nome, $quantidade, $valor)){
+            $objProduto->redirect('../produto.php');
+        }
+    }
+
+    if(isset($_POST['delete'])){
+        $id = $_POST['txtid'];
+        if($objProduto->deletar($id)){
+            $objProduto->redirect('../produto.php');
+        }
+    };
+
+    if(isset($_POST['editar'])){
+        $nome = $_POST['txtnome'];
+        $quantidade = $_POST['txtquantidade'];
+        $valor = $_POST['txtvalor'];
+        $id = $_POST['txtid'];
+        if($objProduto->editar($nome, $quantidade, $valor)){
+            $objProduto->redirect('../produto.php');
+        }
+    }
+?>
---
+++ projeto final/navegacao.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-md bg-dark navbar-dark top-fixed">
+  <a class="navbar-brand" href="#">MENU</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/projeto%20final/cliente.php#">Cliente</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/projeto%20final/funcionario.php#">Funcionario</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/projeto%20final/produto.php#">Produtos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/projeto%20final/vendas.php#">Vendas</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+<br>
